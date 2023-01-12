f5703b45b48f1c4ac75b8313e8d8d00409684aad
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2023-01-13 00:25:34 +0100

    Update for Kadras organization

--- test/test.sh
+++ test/test.sh
@@ -18,7 +18,7 @@ kapp deploy -a kapp-controller -y \
   -f https://github.com/vmware-tanzu/carvel-kapp-controller/releases/latest/download/release.yml
 
 kapp deploy -a kadras-repo -y \
-  -f https://github.com/arktonix/kadras-packages/releases/latest/download/package-repository.yml
+  -f https://github.com/kadras-io/kadras-packages/releases/latest/download/package-repository.yml
 
 kapp deploy -a test-dependencies -f test/test-dependencies -y
 
